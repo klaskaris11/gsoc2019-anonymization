20ba4913a66a9016aa32525586312a7a25c75e6c
chris <xristosandroid@gmail.com>
2020-08-02 20:53:49 +0300

    regex functionality added to excluder.py

--- anonymizer/excluder.py
+++ anonymizer/excluder.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from greek_stemmer import GreekStemmer
 
@@ -6,19 +7,27 @@ from anonymizer.trie_index import prepair_word
 
 
 class Excluder:
-    stemmer = GreekStemmer()
-
     @classmethod
     def exclude(cls, entities):
-        exclude_list = cls.get_exclude_list()
         entities = [x for x in entities if x[1]]
-        clean_entities = [x for x in entities if cls.stemmer.stem(prepair_word(x[1]).upper()) not in exclude_list]
+        entities = cls.stemmer_clean(entities)
+        entities = cls.regex_clean(entities)
+        return entities
+
+    @staticmethod
+    def stemmer_clean(entities):
+        stemmer = GreekStemmer()
+        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'stemmer_exclude_words.txt')
+        with open(file_path, 'r') as file_:
+            exclude_list = file_.read().splitlines()
+        stemmed_excl_list = [stemmer.stem(prepair_word(x).upper()) for x in exclude_list]
+        clean_entities = [x for x in entities if stemmer.stem(prepair_word(x[1]).upper()) not in stemmed_excl_list]
         return clean_entities
 
     @classmethod
-    def get_exclude_list(cls):
-        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'exclude.txt')
+    def regex_clean(cls, entities):
+        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'regex_exclude_words.txt')
         with open(file_path, 'r') as file_:
             exclude_list = file_.read().splitlines()
-        stemmed_exclude_list = [cls.stemmer.stem(prepair_word(x).upper()) for x in exclude_list]
-        return stemmed_exclude_list
+        filtered = [i for i in entities if not any([re.search(patt, i[2]) for patt in exclude_list])]
+        return filtered
