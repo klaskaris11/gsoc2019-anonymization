8a2bb1b04fc6e6a350fe6f61ec77638031222245
klaskaris11 <lolkostis11@hotmail.com>
2020-12-14 22:10:44 +0200

    fixed regex and stemmer, upgrade to bootstrap 4.5.0 and gui improvements

--- src/anonymizer_service/excluder.py
+++ src/anonymizer_service/excluder.py
@@ -14,9 +14,9 @@ class Excluder:
         print(exclude_array)
         entities = [x for x in entities if x[1]]
         print(entities)
-        if exclude_array:
-            entities = cls.stemmer_clean(entities, exclude_array)
-            entities = cls.regex_clean(entities, exclude_array)
+        # if exclude_array:
+        entities = cls.stemmer_clean(entities, exclude_array)
+        entities = cls.regex_clean(entities, exclude_array)
         print(entities)
         return entities
 
@@ -26,10 +26,19 @@ class Excluder:
         exclude_list = exclude_array
         stemmed_excl_list = [stemmer.stem(prepair_word(x).upper()) for x in exclude_list]
         clean_entities = [x for x in entities if stemmer.stem(prepair_word(x[1]).upper()) not in stemmed_excl_list]
+        print("~~~~~~~~~~~~~~~~~STEMMER~~~~~~~~~~~~~~~~~")
+        print("---stemmed_excl_list---")
+        print(stemmed_excl_list)
+        print("---clean_entities---")
+        print(clean_entities)
+        print("~~~~~~~~~~~~~~~~~END OF STEMMER~~~~~~~~~~~~~~~~~")
         return clean_entities
 
     @classmethod
     def regex_clean(cls, entities, exclude_array):
-        exclude_list = exclude_array
+        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'regex_exclude_words.txt')
+        with open(file_path, 'r') as file_:
+            exclude_list = exclude_array + file_.read().splitlines()
+        print(exclude_list)
         filtered = [i for i in entities if not any([re.search(patt, i[2]) for patt in exclude_list])]
         return filtered
